2046a66433f009925594cd3cdde0c5124ab171b1
Matt Sárdi <sardi.matt@gmail.com>
2023-03-17 12:21:48 +0100

    feat(jekyll): slugify library urls as not all filenames are valid urls, clear output dir on script run

--- utils/copy_content_to_jekyll.py
+++ utils/copy_content_to_jekyll.py
@@ -1,12 +1,46 @@
+import re
+import unicodedata
+from collections import defaultdict
+
 import config
 from constants import PROJECT_ROOT
+
+
+# From:
+# https://github.com/django/django/blob/4b1bfea2846f66f504265cec46ee1fe94ee9c98b/django/utils/text.py#L420
+def slugify(value: str) -> str:
+    value = str(value)
+    value = (
+        unicodedata.normalize("NFKD", value).encode("ascii", "ignore").decode("ascii")
+    )
+    value = re.sub(r"[^\w\s-]", "", value.lower())
+    return re.sub(r"[-\s]+", "-", value).strip("-_")
+
+
+def check_if_filenames_cause_duplicates():
+    input_dir = PROJECT_ROOT / "content"
+
+    counter = defaultdict(list)
+    for python_file in input_dir.glob("*.py"):
+        stem = python_file.stem
+        slug = slugify(stem)
+        counter[slug].append(python_file.name)
+        assert len(counter[slug]) < 2, (
+            f"Duplicate names found, {counter[slug]} all map to '{slug}'. Please "
+            f"change a filename."
+        )
 
 
 def copy_content_to_jekyll():
     input_dir = PROJECT_ROOT / "content"
     output_dir = PROJECT_ROOT / config.JEKYLL_ROOT / "libraries"
 
+    check_if_filenames_cause_duplicates()
+
     output_dir.mkdir(exist_ok=True)
+    for markdown_file in output_dir.glob("*.markdown"):
+        markdown_file.unlink()
+    print(f"Cleared '{output_dir}'.")
 
     for python_file in input_dir.glob("*.py"):
         python_content = python_file.read_text()
@@ -19,7 +53,7 @@ layout: page
 {python_content}
 ```
 """
-        output_file = output_dir / f"{python_file.stem}.markdown"
+        output_file = output_dir / f"{slugify(python_file.stem)}.markdown"
         output_file.write_text(result)
         print(f"Written to '{output_file}'.")
 
